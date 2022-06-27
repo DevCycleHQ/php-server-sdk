4dd1011f7bbf12daf5a995b66937955d0a1077d6
Emma Willis <ehenriks@uwo.ca>
2022-06-27 17:08:04 -0400

    Add DVCOptions with enableEdgeDB flag

--- examples/hello-world/app.php
+++ examples/hello-world/app.php
@@ -1,12 +1,12 @@
 <?php
 require_once(__DIR__ . '/vendor/autoload.php');
 
-
 // Configure API key authorization
 $config = DevCycle\Configuration::getDefaultConfiguration()->setApiKey('Authorization', '<INSERT SDK KEY>');
-
+$options = new DevCycle\Model\DVCOptions(true);
 $apiInstance = new DevCycle\Api\DVCClient(
-    $config
+    $config,
+    dvcOptions:$options
 );
 $user_data = new \DevCycle\Model\UserData(array(
     "user_id"=>"user"
