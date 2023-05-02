9a8e8301c0cf5acf3a2a7123a56a7c1aaa712c91
Kaushal Kapasi <kaushal.kapasi@taplytics.com>
2023-05-02 16:07:43 -0400

    [release] 1.0.6

--- lib/Model/UserData.php
+++ lib/Model/UserData.php
@@ -274,7 +274,7 @@ class UserData implements ModelInterface, ArrayAccess, \JsonSerializable
         $this->container['platform'] = 'PHP';
         $this->container['platform_version'] = PHP_VERSION;
         $this->container['sdk_type'] = 'server';
-        $this->container['sdk_version'] =  '1.0.5';
+        $this->container['sdk_version'] =  '1.0.6';
     }
 
     /**
