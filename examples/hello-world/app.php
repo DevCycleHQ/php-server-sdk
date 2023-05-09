d461d6daa01adb2d2adcba2c2556e7e1c7cb824d
Jamie Sinn <jamie.sinn@taplytics.com>
2023-05-09 10:05:12 -0400

    example code for socket

--- examples/hello-world/app.php
+++ examples/hello-world/app.php
@@ -1,15 +1,23 @@
 <?php
+
+use DevCycle\Model\UserData;
+
 require_once(__DIR__ . '/vendor/autoload.php');
 
 // Configure API key authorization
-$config = DevCycle\Configuration::getDefaultConfiguration()->setApiKey('Authorization', '<INSERT SDK KEY>');
-$options = new DevCycle\Model\DVCOptions(true);
+$config = DevCycle\Configuration::getDefaultConfiguration()
+    ->setApiKey('Authorization', 'SDK KEY HERE')
+    ->setHost("http:/v1")
+   ->setUDSPath("/tmp/phpsock.sock");
+$options = new DevCycle\Model\DVCOptions(false);
 $apiInstance = new DevCycle\Api\DVCClient(
     $config,
     dvcOptions: $options
 );
-$user_data = new \DevCycle\Model\UserData(array(
-    "user_id" => "php_test_user"
+$user_data = new UserData(array(
+    "user_id" => "test"
 ));
 
 echo $user_data->__toString();
+
+echo $apiInstance->variable($user_data, "test", false);
