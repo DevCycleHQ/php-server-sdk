76140abcfdec38bf1a02e1f40d1666315bcc2ead
Jamie Sinn <jamie.sinn@taplytics.com>
2023-05-09 11:07:35 -0400

    Use env var for SDK Key

--- examples/hello-world/app.php
+++ examples/hello-world/app.php
@@ -6,7 +6,7 @@ require_once(__DIR__ . '/vendor/autoload.php');
 
 // Configure API key authorization
 $config = DevCycle\Configuration::getDefaultConfiguration()
-    ->setApiKey('Authorization', 'SDK KEY HERE');
+    ->setApiKey('Authorization', $_ENV["DVC_SERVER_SDK_KEY"]);
     // Uncomment the below lines to use unix domain sockets:
     //->setHost("http:/v1")
     //->setUDSPath("/tmp/phpsock.sock");
