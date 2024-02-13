1de5e5fa948e06f3375685dad461582d95747a10
Dakota Washok <dakotawashok@gmail.com>
2024-02-12 22:33:31 -0600

    refactor: update instantiation of DevCycleClient to use new options documentation in readme

--- examples/hello-world-http-proxy/app.php
+++ examples/hello-world-http-proxy/app.php
@@ -2,21 +2,21 @@
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
-use DevCycle\DevCycleConfiguration;
 use DevCycle\Api\DevCycleClient;
 use DevCycle\Model\DevCycleOptions;
 use DevCycle\Model\DevCycleUser;
 
 // Configure API key authorization
-$config = DevCycleConfiguration::getDefaultConfiguration()
-    ->setApiKey('Authorization', getenv("DEVCYCLE_SDK_KEY"))
-    ->setHost("http://localhost:8099");
+$options = new DevCycleOptions(
+    enableEdgeDB: false,
+    bucketingApiHostname: "http://localhost:8099"
+);
 
-$options = new DevCycleOptions(false);
 $apiInstance = new DevCycleClient(
-    $config,
+    getenv("DEVCYCLE_SDK_KEY"),
     dvcOptions: $options
 );
+
 $user_data = new DevCycleUser(array(
     'user_id' => 'integration-test',
     'email' => 'nhaynes@adaction.com',
--- examples/hello-world-udp-proxy/app.php
+++ examples/hello-world-udp-proxy/app.php
@@ -7,17 +7,17 @@ use DevCycle\Api\DevCycleClient;
 use DevCycle\Model\DevCycleOptions;
 use DevCycle\Model\DevCycleUser;
 
-// Configure API key authorization
-$config = DevCycleConfiguration::getDefaultConfiguration()
-    ->setApiKey('Authorization', getenv("DEVCYCLE_SDK_KEY"))
-    ->setHost("http:/v1")
-    ->setUDSPath("/tmp/dvc_lb_proxy.sock");
+$options = new DevCycleOptions(
+    enableEdgeDB: false,
+    bucketingApiHostname: "http://localhost",
+    unixSocketPath: "/tmp/dvc_lb_proxy.sock",
+);
 
-$options = new DevCycleOptions(false);
 $apiInstance = new DevCycleClient(
-    $config,
+    getenv("DEVCYCLE_SDK_KEY"),
     dvcOptions: $options
 );
+
 $user_data = new DevCycleUser(array(
     'user_id' => 'integration-test',
     'email' => 'nhaynes@adaction.com',
