0c089def0e3bf4e8197285384e93ccef2e703550
LenaDooms <lenadooms@hotmail.com>
2024-12-16 10:18:52 +0100

    fix: make implicitly nullable parameters explicit

--- lib/Model/DevCycleEvent.php
+++ lib/Model/DevCycleEvent.php
@@ -165,10 +165,10 @@ class DevCycleEvent implements ModelInterface, ArrayAccess, \JsonSerializable
     /**
      * Constructor
      *
-     * @param mixed[] $data Associated array of property values
+     * @param mixed[]|null $data Associated array of property values
      *                      initializing the model
      */
-    public function __construct(array $data = null)
+    public function __construct(?array $data = null)
     {
         $this->container['type'] = $data['type'] ?? null;
         $this->container['target'] = $data['target'] ?? null;
--- lib/Model/Variable.php
+++ lib/Model/Variable.php
@@ -189,10 +189,10 @@ class Variable implements ModelInterface, ArrayAccess, \JsonSerializable
     /**
      * Constructor
      *
-     * @param mixed[] $data Associated array of property values
+     * @param mixed[]|null $data Associated array of property values
      *                      initializing the model
      */
-    public function __construct(array $data = null)
+    public function __construct(?array $data = null)
     {
         $this->container['_id'] = $data['_id'] ?? null;
         $this->container['key'] = $data['key'] ?? null;
